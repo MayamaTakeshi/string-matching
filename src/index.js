6c390c2c3bc2fff9a4b5d6f28094500baf6a1136
MayamaTakeshi <mayamatakeshi@gmail.com>
2017-12-15 07:25:02 +0900

    Change to throw errors using 'new Error()'

--- src/index.js
+++ src/index.js
@@ -19,20 +19,20 @@ var _set_key = (step, val, dict) => {
 			v = parseInt(val, 8)
 			break
 		default:
-			throw "Unexpected step.type='" + step.type + "'"
+			throw new Error("Unexpected step.type='" + step.type + "'")
 		}
-		if(isNaN(v)) throw "Invalid value for key '" + step.name + "'"
+		if(isNaN(v)) throw new Error("Invalid value for key '" + step.name + "'")
 	}
 
 	if(dict[step.name]) {
-		if(dict[step.name] != v) throw step.name + " value cannot be set to " + v + " because it is already set to " + dict[step.name]
+		if(dict[step.name] != v) throw new Error(step.name + " value cannot be set to " + v + " because it is already set to " + dict[step.name])
 	} else {
 		dict[step.name] = v
 	}
 }
 
 var _match = (steps, received, dict) => {
-	if(typeof received != 'string') throw 'Received element is not string'
+	if(typeof received != 'string') throw new Error('Received element is not string')
 
 	var remainder = received
 	
@@ -41,14 +41,14 @@ var _match = (steps, received, dict) => {
 
 		if(step.op == 'consume') {
 			if(remainder.substr(0, step.str.length) != step.str){
-				throw "Expected substr '" + step.str + "' not found"
+				throw new Error("Expected substr '" + step.str + "' not found")
 			}
 			remainder = remainder.slice(step.str.length)
 		} else if(step.op == 'collect') {
 			var collected_str
 			if(step.length) {
 				collected_str = remainder.substring(0, step.length)
-				if(collected_str.length < step.length) throw "Not enough chars to be collected in element"
+				if(collected_str.length < step.length) throw new Error("Not enough chars to be collected in element")
 					
 				remainder = remainder.slice(step.length)
 			} else {
@@ -57,7 +57,7 @@ var _match = (steps, received, dict) => {
 					var pos = remainder.indexOf(next_step.str)
 					if(pos < 0) {
 						// we dont use (pos <= 0) because it is OK to collect empty strings
-						throw "Expected string collection delimiter '" + next_step.str + "' not found"
+						throw new Error("Expected string collection delimiter '" + next_step.str + "' not found")
 					}
 					collected_str = remainder.substring(0, pos)
 					remainder = remainder.slice(collected_str.length)
@@ -69,7 +69,7 @@ var _match = (steps, received, dict) => {
 			}
 			_set_key(step, collected_str, dict)
 		}	else {
-				throw "Invalid match step"
+				throw new Error("Invalid match step")
 		}
 	}	
 	return true
@@ -81,7 +81,7 @@ var gen_matcher = (expected) => {
 		steps = smp.parse(expected)
 	} catch (e) {
 		console.error(e)
-		throw "Invalid string match expression '" + expected + "'"
+		throw new Error("Invalid string match expression '" + expected + "'")
 	}
 	return (received, dict) => {
 		return _match(steps, received, dict)
